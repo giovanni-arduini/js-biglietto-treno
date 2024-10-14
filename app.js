8e5240a0431b0b1284cd9c1837bff876b5dc4a72
giovanni-arduini <gionnibigudd@gmail.com>
2024-10-14 15:49:12 +0200

    ADD pseudo codice

---
+++ app.js
@@ -0,0 +1,23 @@
+// ACQUISIZIONE DATI
+
+// Chiedere numero km
+// Chiedere età passeggero
+
+// ELABORAZIONE
+
+// Definire classi di sconto
+// Definire prezzo standard
+
+// OUTPUT
+
+// SE passeggero è <18 anni
+//      -> Applicare eventuale sconto minorenni
+// Ricalcolare il prezzo
+
+// ALTRIMENTI SE passeggero >65
+//      -> Applicare sconto over 65
+
+// Ricalcolare il prezzo
+
+// ALTRIMENTI
+// Mostrare prezzo finale
